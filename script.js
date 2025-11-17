b8418de19dbf6359769af519279cf4406d2ab329
guimartins05 <guimartins15@hotmail.com>
2025-11-17 15:39:58 -0300

    Alteração 5

--- script.js
+++ script.js
@@ -1,36 +1,46 @@
-var sw = false;
-
 if("serviceWorker" in navigator){
-  navigator.serviceWorker
-  .register("./sw.js")
-  .then((reg) => {
+    var worker = navigator.serviceWorker;
+    worker.register("./sw.js")
+    .ready()
+    .then((reg) => {
     
-    console.log("Registro de SW Bem-sucedido",reg);
-    sw = true; 
-  })
-  .catch((err) => {
+      console.log("Registro de SW Bem-sucedido",reg);
+     
+    })
+    .catch((err) => {
     
-    console.log("Erro ao tentar registrar SW", err);
-    sw = false;
-  });
+      console.log("Erro ao tentar registrar SW", err);
+    
+    });
 
-  }else{
-   console.log("Não há serviceWorker");
-   sw = false;
-}
 
-console.log("Worker "+sw) 
-
-//if(sw){
-   //console.log("Chegou aqui!"+navigator.serviceWorker.ready) 
-   navigator.serviceWorker.ready.then( registrar =>{
+    worker.ready.then( registrar =>{
 
       //REGISTRAR SYNC A CADA 24HORAS
       registrar.periodicSync.register('check-scheduled-message',{minInterval: 60*1000/*24*60*60*1000*/}/*um dia*/);
       console.log("Chegou aqui!")  
 
+    }).catch((err) => {
+    
+      console.log("Erro ao tentar registrar o Periodic", err);
+      
+    }); 
 
-   });
+
+  }else{
+   console.log("Não há serviceWorker");
+  }
+
+/* if(sw){
+   //console.log("Chegou aqui!"+navigator.serviceWorker.ready) 
+   navigator.serviceWorker.ready.then( registrar =>{
+
+      //REGISTRAR SYNC A CADA 24HORAS
+      //registrar.periodicSync.register('check-scheduled-message',{minInterval: 60*1000/*24*60*60*1000*///}/*um dia*/);
+      //console.log("Chegou aqui!")  
+
+
+   //}); */
 
 //}
 
